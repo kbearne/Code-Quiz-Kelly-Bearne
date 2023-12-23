01866d2f0b59c7323b9d9af208f341e8606079d5
Kelly <{ID}+{username}@users.noreply.github.com>
2023-12-23 12:17:31 +0000

    encapsulation of endCountdown function, amendments to countdown function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,6 +7,8 @@ const startScreenDiv = document.getElementById('start-screen');
 const questionsScreenDiv = document.getElementById('questions');
 const endScreenDiv = document.getElementById('end-screen');
 const feedbackDiv = document.getElementById('feedback');
+// variable for countdown timer function
+var timeInterval = setInterval;
 // variables for game state
 let start = false;
 let finalScore = 0;
@@ -17,18 +19,37 @@ function countdown() {
     timeLeft = 60;
 
     // start timer countdown
-    const timeInterval = setInterval(function() {
+    timeInterval = setInterval(function() {
         timeLeft--;
         timerSpan.innerText = timeLeft;
 
+        // if countdown goes to 0 and quiz is not complete
         if (timeLeft === 0) {
-            start = false;
-            alert("Sorry, you lost! Try again");
-            clearInterval(timeInterval);
-            // go back to start screen
+            // ask the user if they want to try again
+            let tryAgain = confirm("Sorry, time has run out, you lost! Do you want to try again?");
+            // run end countdown function
+            endCountdown();
+
+            // if the user wants to try again; reload quiz, else take the user back to the start page
+            if (tryAgain) {
+                // TODO: not loading first question
+                startQuiz();
+            } else {
+                alert("Thank you for playing");
+                startScreenDiv.classList.toggle("show");
+                // TODO: still showing first question
+                questionsScreenDiv.classList.toggle("hide");
+            }
         }
     }, 1000);
 };
+
+// end the countdown and clear it
+function endCountdown() {
+    start = false;
+    clearInterval(timeInterval);
+    // go back to start screen
+}
 
 // start quiz (display first question)
 function startQuiz() {
@@ -73,14 +94,15 @@ function startQuiz() {
 
 // end quiz after last question (display end screen)
 function endQuiz() {
-    // stop timer
-
     // hide questions screen and show end screen
     questionsScreenDiv.classList.toggle("hide");
     endScreenDiv.classList.toggle("show");
 
     // display final score
     finalScoreSpan.innerText = finalScore
+
+    // end countdown
+    endCountdown();
 
     // allow user to enter their initials
 }
